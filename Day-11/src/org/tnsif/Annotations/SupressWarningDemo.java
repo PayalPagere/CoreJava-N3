0b236d661c768e3b801bd840645e031463261b6f
harshadkasar <harshadkasar@localhost>
2023-02-17 20:18:32 +0530

    Commit one

---
+++ Day-11/src/org/tnsif/Annotations/SupressWarningDemo.java
@@ -0,0 +1,17 @@
+package org.tnsif.Annotations;
+
+import java.util.Scanner;
+
+public class SupressWarningDemo {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		// use to remove warnings
+		Scanner s=new Scanner(System.in);
+		int x=s.nextInt();
+		System.out.println(x);
+		
+
+	}
+
+}
